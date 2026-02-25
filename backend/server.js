b6e5c1031ef87101594f3d8b079fe5b7f4f7c7db
Bhanupratap8766 <bhanupratap8766@gmail.com>
2026-02-25 10:21:53 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -27,25 +27,10 @@ mongoose
   });
 
 /* ================= MIDDLEWARE ================= */
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://ai-powered-project-management-system.onrender.com",
-  "https://project-task-management-system-vcl8.onrender.com"
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+app.use(cors({
+  origin: true,
   credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+}));
 
 
 app.use(express.json());
